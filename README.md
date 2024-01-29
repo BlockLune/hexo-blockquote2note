a93a1a15cf6203c27abd995229521a0387e4b3ed
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-01-29 18:29:28 +0800

    build: hexo plugin starter template project
    Based on https://github.com/hexojs/hexo-tag-embed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hexo-blockquote2note
+
 A Hexo plugin that renders blockquotes to note tag in NexT theme.
---
+++ lib/index.ts
@@ -0,0 +1,5 @@
+// @ts-ignore
+hexo.extend.filter.register('before_post_render', data => {
+  // do some operation on data ...
+  return data;
+});
