aecb0583ab5db0f72154211269a33a66cbfc108a
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-01-30 23:26:01 +0800

    test: for blockquoteLines2NoteLines

---
+++ test/blockquote.test.ts
@@ -0,0 +1,51 @@
+import { assert } from 'chai';
+import { blockquoteLines2NoteLines } from '../lib/blockquote';
+
+
+describe('Lines Transformation Tests', () => {
+  it('should work when blockquote has no config', () => {
+    const exampleLinesNoConfig = [
+      '<!--blockquote2note-->',
+      '> This is Line 1 of this blockquote.',
+      '<!--end-blockquote2note-->'
+    ];
+    const result = blockquoteLines2NoteLines(exampleLinesNoConfig);
+    const expectedResult = [
+      '{% note %}',
+      'This is Line 1 of this blockquote.',
+      '{% endnote %}'
+    ];
+    assert.deepStrictEqual(result, expectedResult);
+  });
+
+  it('should work when blockquote has not full config', () => {
+    const exampleLinesNotFullConfig = [
+      '<!--blockquote2note:,,Test-->',
+      '> This is Line 1 of this blockquote.',
+      '<!--end-blockquote2note-->'
+    ];
+    const result = blockquoteLines2NoteLines(exampleLinesNotFullConfig);
+    const expectedResult = [
+      '{% note Test %}',
+      'This is Line 1 of this blockquote.',
+      '{% endnote %}'
+    ];
+    assert.deepStrictEqual(result, expectedResult);
+  });
+
+  it('should work when blockquote has full config', () => {
+    const exampleLinesFullConfig = [
+      '<!--blockquote2note:default,no-icon,Test-->',
+      '> This is Line 1 of this blockquote.',
+      '<!--end-blockquote2note-->'
+    ];
+    const result = blockquoteLines2NoteLines(exampleLinesFullConfig);
+    const expectedResult = [
+      '{% note default no-icon Test %}',
+      'This is Line 1 of this blockquote.',
+      '{% endnote %}'
+    ];
+    assert.deepStrictEqual(result, expectedResult);
+  });
+});
+
