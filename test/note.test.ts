40c0be1d2f10859cce65989800bc5e26949c57ee
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-01-30 17:37:52 +0800

    test: for note.ts

---
+++ test/note.test.ts
@@ -0,0 +1,33 @@
+import { assert, expect } from 'chai';
+import { getNoteHead } from '../lib/note';
+
+describe('Note Head Tests', () => {
+  it('should work when given full options (all valid)', () => {
+    const fullOptions = 'default,no-icon,Test';
+    const result = getNoteHead(fullOptions);
+    const expectedResult = '{% note default no-icon Test%}';
+    assert.strictEqual(result, expectedResult);
+  });
+
+  it('should work when given partial options (all valid)', () => {
+    const partialOptions = ',,Test';
+    const result = getNoteHead(partialOptions);
+    const expectedResult = '{% note Test %}'; assert.strictEqual(result, expectedResult);
+  });
+
+  it('should work when given no options', () => {
+    const result = getNoteHead();
+    const expectedResult = '{% note %}';
+    assert.strictEqual(result, expectedResult);
+  });
+
+  it('should throw error when given an invalid note type', () => {
+    const invalidNoteType = 'invalidNoteType,,';
+    expect(getNoteHead(invalidNoteType)).to.throw(SyntaxError);
+  });
+
+  it('should throw error when given an invalid icon type', () => {
+    const invalidIconType = ',invalidIconType,';
+    expect(getNoteHead(invalidIconType)).to.throw(SyntaxError);
+  });
+});
